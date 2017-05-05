75169b0d414953c305f5978d4f5c6ec245364e3e
joshuaOrellana <joshua.oc91@gmail.com>
2017-05-05 16:47:21 +0200

    Mobile TPV
    Se ha juntado todos los proyectos de android a uno solo.

---
+++ Mobile_TPV/app/src/main/java/com/joshuaorellana/mobile_tpv/View/SelectedTable.java
@@ -0,0 +1,66 @@
+package com.joshuaorellana.mobile_tpv.View;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.joshuaorellana.mobile_tpv.R;
+
+public class SelectedTable extends AppCompatActivity {
+
+    private ImageView imgTable;
+    private Button btAddOrder;
+    private Button btViewOrder;
+    private Button btModifyOrder;
+    private Button btCloseOrder;
+    private Button btExit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selected_table);
+
+        initComponents();
+
+    }
+
+    private void initComponents() {
+
+        imgTable = (ImageView) findViewById(R.id.imgTable);
+        btAddOrder = (Button) findViewById(R.id.btnAddOrder);
+        btViewOrder = (Button) findViewById(R.id.btnViewOrder);
+        btModifyOrder = (Button) findViewById(R.id.btnModifyOrder);
+        btExit = (Button) findViewById(R.id.btExit);
+
+        Bundle extras = getIntent().getExtras();
+        Bitmap bmp = (Bitmap) extras.getParcelable("imgButton");
+
+        imgTable.setImageBitmap(bmp);
+
+        setListeners();
+
+    }
+
+    private void setListeners() {
+
+        btAddOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent auxIntent = new Intent(SelectedTable.this, AddOrder.class);
+                startActivity(auxIntent);
+            }
+        });
+
+        btExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+    }
+}
